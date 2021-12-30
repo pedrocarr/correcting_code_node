a9438e3b728b83afc2d08477fb2f56ddc27a6c66
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-12-30 14:24:40 -0300

    chore: add flag --inspect

--- src/index.js
+++ src/index.js
@@ -23,14 +23,16 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findindex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
